27273331e2800184a65c02d7a62053f14922c7a3
Dipanjan Biswas <dipanjan.aws.dev@gmail.com>
2024-05-06 09:27:26 +0530

    variables updated

--- Network_Configuration/non-prod/vars.tf
+++ Network_Configuration/non-prod/vars.tf
@@ -28,25 +28,25 @@ variable "region"  {
 variable "vpc_cidr_block"  {
     description = "This represents vpc cidr block"
     type        = string
-    default     = ""
+    default     = "10.0.100.0/22"
 }
 variable "private_subnet_cidr_block_1"  {
     description = "This represents private subnet 1 cidr block"
     type        = string
-    default     = ""
+    default     = "10.0.100.0/24"
 }
 variable "private_subnet_cidr_block_2"  {
     description = "This represents private subnet 2 cidr block"
     type        = string
-    default     = ""
+    default     = "10.0.101.0/24"
 }
 variable "public_subnet_cidr_block_1"  {
     description = "This represents public subnet 1 cidr block"
     type        = string
-    default     = ""
+    default     = "10.0.102.0/24"
 }
 variable "public_subnet_cidr_block_2"  {
     description = "This represents public subnet 2 cidr block"
     type        = string
-    default     = ""
+    default     = "10.0.103.0/24"
 }
